092646b544261b79a80a74531a8be2a5332e835f
Bharath Marambeed <94624923+Bharath5551@users.noreply.github.com>
2025-07-29 10:51:45 +0530

    Create auth.js

---
+++ frontend/js/auth.js
@@ -0,0 +1,6 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+  alert("Login logic to be connected with backend here.");
+});
